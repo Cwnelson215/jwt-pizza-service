d7799bcde9bdd4cae083d27b8db5a89b7f406156
Carter Nelson <cartern215@gmail.com>
2026-01-27 16:33:04 -0700

    updated test

--- src/service.test.js
+++ src/service.test.js
@@ -12,7 +12,7 @@ beforeAll(async () => {
 });
 
 test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
+  const loginRes = await request(app).put('/api/auth/login').send(testUser);
   expect(loginRes.status).toBe(200);
   expectValidJwt(loginRes.body.token);
 
