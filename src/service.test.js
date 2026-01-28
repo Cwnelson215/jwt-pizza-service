4e3df5d43d5933b8ba101bda9982f9c0db340eb3
Carter Nelson <cartern215@gmail.com>
2026-01-27 18:07:56 -0700

    working on tests

--- src/service.test.js
+++ src/service.test.js
@@ -11,6 +11,15 @@ beforeAll(async () => {
   expectValidJwt(testUserAuthToken);
 });
 
+test('register - password too short should fail', async () => {
+  const res = await request(app).post('/api/auth').send({
+    name: 'test user',
+    email: Math.random().toString(36).substring(2, 12) + '@test.com',
+    password: ''
+  });
+  expect(res.status).toBe(400);
+});
+
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth/').send(testUser);
   expect(loginRes.status).toBe(200);
@@ -24,3 +33,41 @@ test('login', async () => {
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
+
+test('logout', async () => {
+  const logoutTestUser = {
+    name: 'logout tester',
+    email: Math.random().toString(36).substring(2, 12) + '@test.com',
+    password: 'logout123',
+  };
+  const registerRes = await request(app).post('/api/auth').send(logoutTestUser);
+  const logoutToken = registerRes.body.token;
+
+
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${logoutToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toContain('logout successful');
+});
+
+// test('create a franchise', async () => {
+//   const franchisee = {
+//     name: 'admin franchiser',
+//     email: Math.random().toString(36).substring(2, 12) + '@test.com',
+//     password: 'admin123',
+//     roles: [{role: 'franchisee'}]
+//   };
+
+//   const franRegisterRes = await request(app).post('api/auth').send(franchisee);
+//   const franLoginRes =  await request(app).put('api/auth/').send({
+//     email: franchisee.email,
+//     password: franchisee.password
+//   });
+
+//   const franToken = franLoginRes.body.token;
+
+//   const franchiseRequest = {
+//     name:'Pizza Palace',
+//     admins:
+//   }
+
+// });
